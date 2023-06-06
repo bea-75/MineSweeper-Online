a8383ec968465c8bb01e12db5b2dcdbe01997a98
bea-75 <129797236+bea-75@users.noreply.github.com>
2023-06-06 09:36:41 -0700

    Update minesweeper.js

--- static/minesweeper.js
+++ static/minesweeper.js
@@ -1,127 +1,87 @@
-.add {
-	margin: 20px;
-}
+$(document).ready(function() {
 
-.btn-light {
-	color: black;
-}
+	var checked = false;
+	var game_end = false;
 
-.pfp {
-	border-radius: 50%;
-}
+	$(".bomb-num").load("bomb_num")
 
-span {
-	display: inline;
-	width: 100px;
-	height: 100px;
-	padding: 16px;
-	background-color: #44ad45; 
-}
+	$(".block").click(function() {
+		if ($('button').hasClass('check')){
+			checked = true;
+		} else {
+			checked = false;
+		}
 
-.block {
-	width: 45px;
-	height: 55px;
-	padding: 0px;
-	color: #198754;
-	/*background-color: #198754;*/
-	border-color: #198754;
-	border-radius: 8px;
-}
 
-.block:hover {
-	color: #157348;
-	background-color: #157348;
-}
+		if (checked == false) {
+			$(this).addClass("check");
+			$(this).removeClass("block");
+		}
+		if (checked == true) {
+			$(this).removeClass("check");
+			$(this).addClass("block");
+		}
+	});
 
-.check:hover {
-	color: #00de00;
-	background-color: #00de00;
-}
+	$(".flag").click(function() {
+		if ($('button').hasClass('check') && $('.check').hasClass('flagged')) {
+			$('.check').load('unflag')
+			$('.check').addClass("block");
 
-.destroyed:hover {
-	color: #cfc4ab;
-	background-color: #cfc4ab;
-}
+			if ($(".check").hasClass("bomb")) {
+				$('.check').removeClass("flagged-right");
+			}
+			else {
+				$('.check').removeClass("flagged-wrong");
+			}
 
-.destroyed-0:hover {
-	color: #cfc4ab;
-	background-color: #cfc4ab;
-}
+			$('.check').removeClass("flagged");
+			$('.check').removeClass("check");
+			checked = false;
+		}
+		else if ($('button').hasClass('check')) {
+			$('.check').load('flag')
+			$('.check').addClass("block");
+			$('.check').addClass("flagged");
 
-.check {
-	padding: 0px;
-	width: 45px;
-	height: 50px;
-	background-color: #00ff00;
-	color: #00ff00;
-	border-color: #00ff00;
-	border-radius: 8px;
-}
+			if ($(".check").hasClass("bomb")) {
+				$('.check').addClass("flagged-right");
+			}
+			else {
+				$('.check').addClass("flagged-wrong");
+			}
 
-.destroyed {
-	padding: 0px;
-	width: 45px;
-	height: 50px;
-	background-color: #ebdfc3;
-	color: black;
-	border-color: #ebdfc3;
-	border-radius: 8px;
-	font-size: 18px;
-}
+			$('.check').removeClass("check");
+			checked = false;
+		}
+	});
 
-.destroyed-0 {
-	padding: 0px;
-	width: 45px;
-	height: 50px;
-	background-color: #ebdfc3;
-	color: #ebdfc3;
-	border-color: #ebdfc3;
-	border-radius: 8px;
-	font-size: 18px;
-}
-
-.exploded {
-	padding: 0px;
-	width: 45px;
-	height: 50px;
-	background-color: #ff0000;
-	color: #ff0000;
-	border-color: #ff0000;
-}
-
-.correct {
-	padding: 0px;
-	width: 45px;
-	height: 50px;
-	background-color: #00e016;
-	color: #00e016;
-	border-color: #00e016;
-}
-
-.wrong {
-	padding: 0px;
-	width: 45px;
-	height: 50px;
-	background-color: #ffd900;
-	color: #ffd900;
-}
-
-.play {
-	background-color: #44ad45;
-}
-
-.selector {
-	background-color: #44ad45;
-	color: #44ad45;
-	border-color: #44ad45;
-	border-radius: 8px;
-}
-
-.centered {
-	text-align: center;
-}
-
-.bomb-num {
-	font-size: 20px;
-	color: white;
-}
+	$(".destroy").click(function(){
+		if ($('button').hasClass('check') && $(".check").hasClass("bomb")) {
+			$('.bomb').load('bomb')
+			$(".bomb").removeClass("check");
+			$('.bomb').addClass('exploded');
+			if ($('.bomb').hasClass('flagged-right')) {
+				$(".flagged-right").load('flag')
+				$(".flagged-right").addClass("correct");
+			}	
+			if ($(".block").hasClass("flagged-wrong")) {
+				$(".flagged-wrong").load('flag')
+				$(".flagged-wrong").addClass("wrong");
+			}
+			setTimeout(function(){
+				$('.game').load('game_end_lose')
+			}, 1000);
+		}
+		else if ($('button').hasClass('check') && $(".check").hasClass("0")) {
+			$('.0').addClass("destroyed-0");
+			$('.check').removeClass("check");
+			checked = false;
+		}
+		else if ($('button').hasClass('check')) {
+			$('.check').addClass("destroyed");
+			$('.check').removeClass("check");
+			checked = false;
+		}
+	});
+});
